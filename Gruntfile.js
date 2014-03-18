bda9ff52bafc7a32971bf54742f765d78be8da0f
Diya Khan <diya_khan_me@hotmail.com>
2014-03-18 13:03:11 +0000

    wip Facade Validations

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.registerTask("test", ["jshint",  "mochaTest"]);
+    grunt.registerTask("test", ["mochaTest"]);
     grunt.registerTask("default", ["watch:default"]);
 };
 
--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,31 @@
 // Mocha tests should be created
 
+require("../components/index")();
+var chai = require("chai"),
+    expect = chai.expect;
+var BaseFacade = require("../lib/facade/base");
 
+describe("Validation", function() {
 
+    it("returns a number", function() {
+        for (var i = 0; i < 10; i++) {
+            expect(i).to.be.a("number");
+        }
+    });
 
+    it("test", function() {
+        
+        var UserFacade = BaseFacade.extend();
+        
+        UserFacade.prototype.get = function get(input){
+            this.rules = [{ "user_id" : ["required"] }];
+            this.validate(input);
+        };
+        
+        var params = { "user_id" : 1 };
+        var uf = new UserFacade();
+        uf.get(params);
+        
+    });
 
+});
