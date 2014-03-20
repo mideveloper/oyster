df4385eff511fa185c2d03df2a5995661c0b73b6
Syed Azfar Ali <azf.scorpy@gmail.com>
2014-03-20 08:11:04 -0700

    saveInBatch function added in mongo

--- out/api.js
+++ out/api.js
@@ -1,9 +1,18 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "Crypt",
         "Epoch"
     ],
-    "modules": [],
-    "allModules": []
+    "modules": [
+        "Util"
+    ],
+    "allModules": [
+        {
+            "displayName": "Util",
+            "name": "Util",
+            "description": "This is the description for my class."
+        }
+    ]
 } };
 });
