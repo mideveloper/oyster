2ea913bf489d5fe1ecb8a60c3c18bf42cd055754
Syed Azfar Ali <azf.scorpy@gmail.com>
2014-04-02 13:34:23 -0700

    test fixes

--- tests/functional/cache.js
+++ tests/functional/cache.js
@@ -3,71 +3,71 @@ var chai = require("chai"),
     expect = chai.expect;
 
 require("../../components")();
-   
+
 chai.use(chaiAsPromised);
 
 
 
-describe("Cache", function () {
-    
+describe("Cache", function() {
+
     var cache = require("../../lib/utils/cache");
     var local_cacheObj, memcache_Obj;
 
-    before(function () {
+    before(function() {
         local_cacheObj = cache.initialize({
             client: "local"
         });
         memcache_Obj = cache.initialize({
-            client : "memcache"
+            client: "memcache"
         });
-        
+
         console.log("init");
-        
+
     });
 
-    after(function () {
-        
+    after(function() {
+
     });
-    
-    it("setAndGetLocalCache", function () {
-        return local_cacheObj.set("abc", "bcd").then(function(){
-          return local_cacheObj.get("abc").then(function(val){
-            return val;
-          });
-        }).then(function(val){
+
+    it("setAndGetLocalCache", function() {
+        return local_cacheObj.set("abc", "bcd").then(function() {
+            return local_cacheObj.get("abc").then(function(val) {
+                return val;
+            });
+        }).then(function(val) {
             expect(val).to.equal("bcd");
         });
     });
-    
-    it("removeLocalCache", function () {
-        return local_cacheObj.remove("abc").then(function(){
-          return local_cacheObj.get("abc").then(function(val){
-            return val;
-          });
-        }).then(function(val){
+
+    it("removeLocalCache", function() {
+        return local_cacheObj.remove("abc").then(function() {
+            return local_cacheObj.get("abc").then(function(val) {
+                return val;
+            });
+        }).then(function(val) {
             expect(val).to.not.exist;
-            
+
         });
     });
 
-    it("setAndGetMemcache", function () {
-        return memcache_Obj.set("abc", "bcd").then(function(){
-          return memcache_Obj.get("abc").then(function(val){
-            return val;
-          });
-        }).then(function(val){
+    it("setAndGetMemcache", function() {
+        return memcache_Obj.set("abc", "bcd").then(function() {
+            return memcache_Obj.get("abc").then(function(val) {
+                return val;
+            });
+        }).then(function(val) {
             expect(val).to.equal("bcd");
         });
     });
-    
-    it("removeMemcache", function () {
-        return memcache_Obj.remove("abc").then(function(){
-          return memcache_Obj.get("abc").then(function(val){
-            return val;
-          });
-        }).then(function(val){
+
+    it("removeMemcache", function() {
+        return memcache_Obj.remove("abc").then(function() {
+            return memcache_Obj.get("abc").then(function(val) {
+                return val;
+            });
+        }).then(function(val) {
             expect(val).to.not.exist;
-            
+
         });
     });
 
