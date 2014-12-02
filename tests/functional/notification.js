ac78c3ca0c1c050b2d0d1350f3f2374f096de410
Syed Azfar Ali <azf.scorpy@gmail.com>
2014-12-02 04:38:34 -0800

    cache and notification change

--- tests/functional/notification.js
+++ tests/functional/notification.js
@@ -1,39 +1,39 @@
-var chai = require("chai"),
-    chaiAsPromised = require("chai-as-promised"),
-    expect = chai.expect;
+// var chai = require("chai"),
+//     chaiAsPromised = require("chai-as-promised"),
+//     expect = chai.expect;
 
-require("../../components")();
+// require("../../components")();
 
-chai.use(chaiAsPromised);
+// chai.use(chaiAsPromised);
 
 
 
-describe("Notification", function() {
-     this.timeout(15000);
-    var Notification = require("../../lib/helpers/notification");
+// describe("Notification", function() {
+//      this.timeout(15000);
+//     var Notification = require("../../lib/helpers/notification");
    
-    var notification;
-    before(function() {
-        notification = new Notification();
-        notification.addIOSClient("../assets/client_stg_consumer_key.pem", "../assets/client_stg_consumer_cert.pem","../assets/client_stg_consumer_key.pem", "../assets/client_stg_consumer_cert.pem");
-        console.log("init");
+//     var notification;
+//     before(function() {
+//         notification = new Notification();
+//         notification.addIOSClient("../assets/client_stg_consumer_key.pem", "../assets/client_stg_consumer_cert.pem","../assets/client_stg_consumer_key.pem", "../assets/client_stg_consumer_cert.pem");
+//         console.log("init");
 
-    });
+//     });
 
-    after(function() {
+//     after(function() {
 
-    });
+//     });
 
-    it("sendNotification", function(done) {
-        notification.send(
-            [{ push_token : "00fd174023674ac38db921af077253c2994aa0bb40a35c9043ad5796cd94caca", platform : "iphone"}], { message : "this is new message"});
+//     it("sendNotification", function(done) {
+//         notification.send(
+//             [{ push_token : "00fd174023674ac38db921af077253c2994aa0bb40a35c9043ad5796cd94caca", platform : "iphone"}], { message : "this is new message"});
         
-        setTimeout(function () {
-            console.log("its done");
-            done();
-        }, 10000);
-    });
+//         setTimeout(function () {
+//             console.log("its done");
+//             done();
+//         }, 10000);
+//     });
 
     
 
-});
+// });
